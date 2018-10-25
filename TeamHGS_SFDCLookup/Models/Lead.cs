e5b5b82c86b09a4186392cf6280462d49481b5a8
Wyatt Conrad <wyattconrad@hotmail.com>
2018-10-25 17:00:37 -0400

    Added Account, Contact, and Lead Models

---
+++ TeamHGS_SFDCLookup/Models/Lead.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TeamHGS_SFDCLookup.Models
+{
+    public class Lead
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Email { get; set; }
+        public string OBU { get; set; }
+        public string Company { get; set; }
+    }
+}
